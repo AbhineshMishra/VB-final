37010275f84cab3deeb9c4306619fe1b24408353
AbhineshMishra <gotoabhinesh@gmail.com>
2015-07-25 13:35:21 +0530

    Changed the comment

--- connection.php
+++ connection.php
@@ -16,6 +16,7 @@ $prefix = "";
 //    {
  //   echo "Connection failed: " . $e->getMessage();
   //  }
+  // Try changing it to NOSQL
 $bd = mysql_connect($mysql_hostname, $mysql_user, $mysql_password) or die("Could not connect database");
 mysql_select_db($mysql_database, $bd) or die("Could not select database");
 
